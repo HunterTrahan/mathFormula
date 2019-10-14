48630af1fd8f19bf8b11739faa786bfe7a9fa643
Trahan.hunter <Trahan.hunter@yahoo.com>
2019-10-14 17:07:49 -0500

    Add project files.

---
+++ mathFormula/Functions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathFormula
+{
+    class Functions
+    {
+        public float BasicQuad (float x)
+        {
+                float result = x * x + 2 * x - 7;
+                Console.WriteLine(result);
+                return result;
+        }
+
+        public double Quad (float a, float b, float c)
+        {
+            double result = -b + Math.Sqrt (b * 2 - 4 * a * c / 2 * a);
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public double BasicBlend (float s, float e, float t)
+        {
+            double result = s + t * (e - s);
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
---
+++ mathFormula/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathFormula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Functions Maths = new Functions();
+
+            int x = 14;
+
+            int a = 2;
+            int b = 4;
+            int c = 6;
+
+            int s = 3;
+            int e = 6;
+            int t = 12;
+
+            Maths.BasicQuad(x);
+
+            Maths.Quad(a, b, c);
+
+            Maths.BasicBlend(s, e, t);
+
+            Console.ReadLine();
+        }
+    }
+}
